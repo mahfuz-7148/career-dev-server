ecb8c0528ce8230dbfa80b374666c7932e8e8aa1
mahfuz-7148 <mrahman7148@gmail.com>
2025-06-01 11:46:11 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
+app.listen(port, () => {
+    console.log(`server ${port}`)
+})
