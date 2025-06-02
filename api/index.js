9c108d59d42463ce850c94edf43efcd3afbdc0de
mahfuz-7148 <mrahman7148@gmail.com>
2025-06-02 23:01:56 +0600

    working

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const express = require('express')
 const cors = require('cors')
@@ -7,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://mrahman7148:M83UyZjXMIm31Abh@careerdev.6rwjfy7.mongodb.net/?retryWrites=true&w=majority&appName=careerDev";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECURITY_KEY}@careerdev.6rwjfy7.mongodb.net/?retryWrites=true&w=majority&appName=careerDev`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
